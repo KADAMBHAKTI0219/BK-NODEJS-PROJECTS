ced3d9fb422fb916cbd97b58e410639b19c5e5de
KADAMBHAKTI0219 <kadambhakti0219@example.com>
2025-01-21 10:02:41 +0530

    complate

--- Convert HTMl to node js/CONVERT HTML TO NODE/db.js
+++ Convert HTMl to node js/CONVERT HTML TO NODE/db.js
@@ -1,5 +1,5 @@
 const mongoose=require("mongoose");
 
-const connect=mongoose.connect("mongodb://127.0.0.1:27017/htmltonode")
+const connect=mongoose.connect("mongodb://127.0.0.1:27017/convert_htmltonode")
 
 module.exports=connect
